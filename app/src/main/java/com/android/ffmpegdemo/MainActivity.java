467ff0cfb662dc1d2c54734f4596ce14b2e402ff
JeffMony <jeffmony@163.com>
2021-07-27 15:07:54 +0800

    增加项目权限申请的逻辑代码
    Signed-off-by: JeffMony <jeffmony@163.com>

--- app/src/main/java/com/android/ffmpegdemo/MainActivity.java
+++ app/src/main/java/com/android/ffmpegdemo/MainActivity.java
@@ -1,14 +1,23 @@
 package com.android.ffmpegdemo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int REQUEST_CODE = 1;
 
     private Button mFFmpegInfoBtn;
     private Button mTransformBtn;
@@ -35,6 +44,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mFFmpegCmdBtn.setOnClickListener(this);
         mCutVideoBtn.setOnClickListener(this);
         mSplitStreamBtn.setOnClickListener(this);
+
+        requestPermissions();
+    }
+
+    private void requestPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "权限申请成功", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "权限申请失败", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
